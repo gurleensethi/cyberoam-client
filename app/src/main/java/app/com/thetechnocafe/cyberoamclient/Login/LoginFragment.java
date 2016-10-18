b360f059bd9097029cf26a634eee9457d67bdccb
gurleensethi <sarusethi@rocketmail.com>
2016-10-18 19:01:13 +0530

    Make login button to image view with circle drawable

--- app/src/main/java/app/com/thetechnocafe/cyberoamclient/Login/LoginFragment.java
+++ app/src/main/java/app/com/thetechnocafe/cyberoamclient/Login/LoginFragment.java
@@ -6,8 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import app.com.thetechnocafe.cyberoamclient.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by gurleensethi on 18/10/16.
@@ -17,6 +21,12 @@ public class LoginFragment extends Fragment implements ILoginView {
 
     private LoginPresenter mLoginPresenter;
 
+    @BindView(R.id.enrollmentEditText)
+    EditText mEnrollmentEditText;
+    @BindView(R.id.passwordEditText)
+    EditText mPasswordEditText;
+    @BindView(R.id.loginButton)
+    ImageView mLoginButton;
 
     public static LoginFragment getInstance() {
         LoginFragment fragment = new LoginFragment();
@@ -28,20 +38,22 @@ public class LoginFragment extends Fragment implements ILoginView {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
 
+        //Bind ButterKnife
+        ButterKnife.bind(this, view);
+
         mLoginPresenter = new LoginPresenter(this);
 
         return view;
     }
 
-
     /**
      * All the on click listeners are set up here
-     * LoginPresenter calls this function on view*/
+     * LoginPresenter calls this function on view
+     */
     @Override
     public void setUpOnClickListeners() {
 
     }
-
 
     /**
      * Handle the login response from presenter
