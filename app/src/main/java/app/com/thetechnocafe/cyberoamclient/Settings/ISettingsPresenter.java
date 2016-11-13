397ce08c22bae19a6562a543b0a6b9d841315f6f
gurleensethi <sarusethi@rocketmail.com>
2016-11-14 03:03:19 +0530

    #Add : Interface and classes for settings

---
+++ app/src/main/java/app/com/thetechnocafe/cyberoamclient/Settings/ISettingsPresenter.java
@@ -0,0 +1,8 @@
+package app.com.thetechnocafe.cyberoamclient.Settings;
+
+/**
+ * Created by gurleensethi on 14/11/16.
+ */
+
+public interface ISettingsPresenter {
+}
---
+++ app/src/main/java/app/com/thetechnocafe/cyberoamclient/Settings/SettingsActivity.java
@@ -0,0 +1,13 @@
+package app.com.thetechnocafe.cyberoamclient.Settings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
