fd26d3d5c7f5aff524f6b69c077c2a527664c5e6
gurleensethi <sarusethi@rocketmail.com>
2016-11-18 23:34:46 +0530

    #Fix : Dialog width

--- app/src/main/java/app/com/thetechnocafe/cyberoamclient/Dialogs/CustomProgressDialog.java
+++ app/src/main/java/app/com/thetechnocafe/cyberoamclient/Dialogs/CustomProgressDialog.java
@@ -1,5 +1,6 @@
 package app.com.thetechnocafe.cyberoamclient.Dialogs;
 
+import android.app.Dialog;
 import android.app.DialogFragment;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -62,9 +64,15 @@ public class CustomProgressDialog extends DialogFragment {
     }
 
     @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        return dialog;
+    }
+
+    @Override
     public void onStart() {
         super.onStart();
-        //Make dialog match parent
         getDialog().getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
     }
 }
--- app/src/main/java/app/com/thetechnocafe/cyberoamclient/Dialogs/NewAccountDialogFragment.java
+++ app/src/main/java/app/com/thetechnocafe/cyberoamclient/Dialogs/NewAccountDialogFragment.java
@@ -1,5 +1,6 @@
 package app.com.thetechnocafe.cyberoamclient.Dialogs;
 
+import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -7,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -100,9 +102,15 @@ public class NewAccountDialogFragment extends DialogFragment {
     }
 
     @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        return dialog;
+    }
+
+    @Override
     public void onStart() {
         super.onStart();
-
         getDialog().getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
     }
 }
